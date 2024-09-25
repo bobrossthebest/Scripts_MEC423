e2f44e1178f358fe550f7933f48823e8fa95126f
bobrossthebest <juliengroulx@gmail.com>
2024-09-25 10:53:30 -0400

    Imports

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# imports
+# imports externes
 
 import numpy as np
 import sympy as sp
 
-import Modules.Flambage
-import Modules.
+# imports internes
+
+import Modules.Treillis_2D as T2d, Modules.Treillis_3D as T3d, Modules.Poutre_1D as P1d, Modules.Charpente_2D as C2d
+import Modules.Surfacique as Sq, Modules.Jeux as Jx, Modules.Ressorts as Rt, Modules.Appui_incliné as App
+import Modules.Mécanisme as Mcn, Modules.Flambage as Flb, Modules.Thermique as Thm
+
