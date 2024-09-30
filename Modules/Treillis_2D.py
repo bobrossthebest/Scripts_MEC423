5060cde78ebf487e82bf41579fe6678b870b50a4
bobrossthebest <juliengroulx@gmail.com>
2024-09-30 19:26:40 -0400

    Modification jusqu'à "conditions aux frontières" pour accepter les dictionnaires

--- Modules/Treillis_2D.py
+++ Modules/Treillis_2D.py
@@ -89,78 +89,72 @@ for i in range(nb_elements):
                                               elements['xj'][i], elements['yj'][i])
 
 
-ddl1 = np.array([1, 2, 3, 4])
-xi1, yi1 = 0, 0
-xj1, yj1 = 800, 600
-E1 = 2e5
-A1 = 24
-alpha1 = 12e-6
-dT1 = 100
-Sy1 = 310
-k1 = calculer_k_barre2d(E1, A1, xi1, yi1, xj1, yj1)
-feq1 = calculer_feq_barre2d(E1, A1, alpha1, dT1, xi1, yi1, xj1, yj1)
-
-ddl2 = np.array([3, 4, 5, 6])
-xi2, yi2 = 800, 600
-xj2, yj2 = 800, 0
-E2 = 2e5
-A2 = 24
-alpha2 = 12e-6
-dT2 = 0
-Sy2 = 310
-k2 = calculer_k_barre2d(E2, A2, xi2, yi2, xj2, yj2)
-feq2 = calculer_feq_barre2d(E2, A2, alpha2, dT2, xi2, yi2, xj2, yj2)
-
-ddl3 = np.array([3, 4, 7, 8])
-xi3, yi3 = 800, 600
-xj3, yj3 = 1600, 0
-E3 = 2e5
-A3 = 24
-alpha3 = 12e-6
-dT3 = 0
-Sy3 = 310
-k3 = calculer_k_barre2d(E3, A3, xi3, yi3, xj3, yj3)
-feq3 = calculer_feq_barre2d(E3, A3, alpha3, dT3, xi3, yi3, xj3, yj3)
-
-ddl4 = np.array([1, 2, 5, 6])
-xi4, yi4 = 0, 0
-xj4, yj4 = 800, 0
-E4 = 2e5
-A4 = 24
-alpha4 = 12e-6
-dT4 = 0
-Sy4 = 310
-k4 = calculer_k_barre2d(E4, A4, xi4, yi4, xj4, yj4)
-feq4 = calculer_feq_barre2d(E4, A4, alpha4, dT4, xi4, yi4, xj4, yj4)
-
-ddl5 = np.array([5, 6, 7, 8])
-xi5, yi5 = 800, 0
-xj5, yj5 = 1600, 0
-E5 = 2e5
-A5 = 24
-alpha5 = 12e-6
-dT5 = 0
-Sy5 = 310
-k5 = calculer_k_barre2d(E5, A5, xi5, yi5, xj5, yj5)
-feq5 = calculer_feq_barre2d(E5, A5, alpha5, dT5, xi5, yi5, xj5, yj5)
+# ddl1 = np.array([1, 2, 3, 4])
+# xi1, yi1 = 0, 0
+# xj1, yj1 = 800, 600
+# E1 = 2e5
+# A1 = 24
+# alpha1 = 12e-6
+# dT1 = 100
+# Sy1 = 310
+# k1 = calculer_k_barre2d(E1, A1, xi1, yi1, xj1, yj1)
+# feq1 = calculer_feq_barre2d(E1, A1, alpha1, dT1, xi1, yi1, xj1, yj1)
+#
+# ddl2 = np.array([3, 4, 5, 6])
+# xi2, yi2 = 800, 600
+# xj2, yj2 = 800, 0
+# E2 = 2e5
+# A2 = 24
+# alpha2 = 12e-6
+# dT2 = 0
+# Sy2 = 310
+# k2 = calculer_k_barre2d(E2, A2, xi2, yi2, xj2, yj2)
+# feq2 = calculer_feq_barre2d(E2, A2, alpha2, dT2, xi2, yi2, xj2, yj2)
+#
+# ddl3 = np.array([3, 4, 7, 8])
+# xi3, yi3 = 800, 600
+# xj3, yj3 = 1600, 0
+# E3 = 2e5
+# A3 = 24
+# alpha3 = 12e-6
+# dT3 = 0
+# Sy3 = 310
+# k3 = calculer_k_barre2d(E3, A3, xi3, yi3, xj3, yj3)
+# feq3 = calculer_feq_barre2d(E3, A3, alpha3, dT3, xi3, yi3, xj3, yj3)
+#
+# ddl4 = np.array([1, 2, 5, 6])
+# xi4, yi4 = 0, 0
+# xj4, yj4 = 800, 0
+# E4 = 2e5
+# A4 = 24
+# alpha4 = 12e-6
+# dT4 = 0
+# Sy4 = 310
+# k4 = calculer_k_barre2d(E4, A4, xi4, yi4, xj4, yj4)
+# feq4 = calculer_feq_barre2d(E4, A4, alpha4, dT4, xi4, yi4, xj4, yj4)
+#
+# ddl5 = np.array([5, 6, 7, 8])
+# xi5, yi5 = 800, 0
+# xj5, yj5 = 1600, 0
+# E5 = 2e5
+# A5 = 24
+# alpha5 = 12e-6
+# dT5 = 0
+# Sy5 = 310
+# k5 = calculer_k_barre2d(E5, A5, xi5, yi5, xj5, yj5)
+# feq5 = calculer_feq_barre2d(E5, A5, alpha5, dT5, xi5, yi5, xj5, yj5)
 
 # ----------
 # Assemblage
 # ----------
 
-Ktot = np.zeros((8, 8))
-Ktot = assembler_matrice(Ktot, k1, ddl1, ddl1)
-Ktot = assembler_matrice(Ktot, k2, ddl2, ddl2)
-Ktot = assembler_matrice(Ktot, k3, ddl3, ddl3)
-Ktot = assembler_matrice(Ktot, k4, ddl4, ddl4)
-Ktot = assembler_matrice(Ktot, k5, ddl5, ddl5)
+Ktot = np.zeros((nb_noeuds * 2, nb_noeuds * 2))
+for i in range(nb_elements):
+    Ktot = assembler_matrice(Ktot, elements['k'][i], elements['ddl'][i], elements['ddl'][i])
 
-Feqtot = np.zeros((8, 1))
-Feqtot = assembler_vecteur(Feqtot, feq1, ddl1)
-Feqtot = assembler_vecteur(Feqtot, feq2, ddl2)
-Feqtot = assembler_vecteur(Feqtot, feq3, ddl3)
-Feqtot = assembler_vecteur(Feqtot, feq4, ddl4)
-Feqtot = assembler_vecteur(Feqtot, feq5, ddl5)
+Feqtot = np.zeros((nb_noeuds * 2, 1))
+for i in range(nb_elements):
+    Feqtot = assembler_vecteur(Feqtot, elements['feq'][i], elements['ddl'][i])
 
 # -------------------------
 # Conditions aux frontieres
