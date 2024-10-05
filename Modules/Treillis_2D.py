f5f3dbe1d4b6de526f034593c7e13d081ef54a3a
bobrossthebest <juliengroulx@gmail.com>
2024-10-05 15:31:56 -0400

    boucle print des forces et contraintes

--- Modules/Treillis_2D.py
+++ Modules/Treillis_2D.py
@@ -217,13 +217,12 @@ for i in range(nb_elements):
                                                elements['xi'][i], elements['yi'][i],
                                                elements['xj'][i], elements['yj'][i])
 
-print(tab_sigma)
-
 tab_force = [0] * nb_elements
 for i in range(nb_elements):
     tab_force[i] = elements['A'][i] * tab_sigma[i]
 
-print(tab_force)
+for i in range(nb_elements):
+    print(f"Élément {i+1}\tForce: {tab_force[i]}{F}\tContrainte: {tab_sigma[i]}{P}")
 
 for i in range(len(Ui)):
     print(f"Déplacement {ddlFc[i]}:\t{Ui[i]}")
