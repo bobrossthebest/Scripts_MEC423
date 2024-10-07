7789638aa2c94ee55ab2be8c0cd0d3ef1c78f6de
bobrossthebest <juliengroulx@gmail.com>
2024-10-07 18:46:19 -0400

    Passage des boucles de Treillis_2D

--- Modules/Treillis_3D.py
+++ Modules/Treillis_3D.py
@@ -1,9 +1,15 @@
-# Unites : lbf-po-psi
-
 import numpy as np
 
 from Modules.Fonctions_partagées import (assembler_matrice, extraire_matrice, extraire_vecteur,
                                          reconstruire_vecteur)
+
+# Unites :
+F = input("Quelle est l'unité de mesure de force")
+L = input("Quelle est l'unité de mesure de longueur")
+P = input("Quelle est l'unité de mesure de contrainte")
+
+# Variables définies dans des boucles
+noeuds, nb_noeuds, nb_elements, elements, ddlFc, ddlUc, Fc, Uc = [False] * 8
 
 
 # ---------
@@ -17,11 +23,11 @@ def calculer_k_barre3d(e, a, xi, yi, zi, xj, yj, zj):
     cy = (yj - yi) / l_
     cz = (zj - zi) / l_
     k = (e * a / l_) * np.array([[cx ** 2, cx * cy, cx * cz, -cx ** 2, -cx * cy, -cx * cz],
-                                [cx * cy, cy ** 2, cy * cz, -cx * cy, -cy ** 2, -cy * cz],
-                                [cx * cz, cy * cz, cz ** 2, -cx * cz, -cy * cz, -cz ** 2],
-                                [-cx ** 2, -cx * cy, -cx * cz, cx ** 2, cx * cy, cx * cz],
-                                [-cx * cy, -cy ** 2, -cy * cz, cx * cy, cy ** 2, cy * cz],
-                                [-cx * cz, -cy * cz, -cz ** 2, cx * cz, cy * cz, cz ** 2]])
+                                 [cx * cy, cy ** 2, cy * cz, -cx * cy, -cy ** 2, -cy * cz],
+                                 [cx * cz, cy * cz, cz ** 2, -cx * cz, -cy * cz, -cz ** 2],
+                                 [-cx ** 2, -cx * cy, -cx * cz, cx ** 2, cx * cy, cx * cz],
+                                 [-cx * cy, -cy ** 2, -cy * cz, cx * cy, cy ** 2, cy * cz],
+                                 [-cx * cz, -cy * cz, -cz ** 2, cx * cz, cy * cz, cz ** 2]])
     return k
 
 
@@ -46,44 +52,196 @@ def calculer_contrainte_barre3d(u_tot, ddl, e, alpha, dt, xi, yi, zi, xj, yj, zj
 # Proprietes de chaque element
 # ----------------------------
 
-ddl1 = np.array([1, 2, 3, 10, 11, 12])
-xi1, yi1, zi1 = 0, 60, 84
-xj1, yj1, zj1 = 24, 60, 48
-E1 = 30e6
-A1 = 0.2
-k1 = calculer_k_barre3d(E1, A1, xi1, yi1, zi1, xj1, yj1, zj1)
+redo = True
+while redo is True:
+    try:
+        nb_noeuds = int(input("Combien de noeuds contient la structure? "))
+    except (ValueError, SyntaxError, TypeError):
+        continue
+    noeuds = {'x': [0] * nb_noeuds, 'y': [0] * nb_noeuds, 'z': [0] * nb_noeuds,
+              'ddlx': [0] * nb_noeuds, 'ddly': [0] * nb_noeuds, 'ddlz': [0] * nb_noeuds, }
+    for i in range(nb_noeuds):
+        # boucle for pour passer à travers les noeuds, boucle while pour valider les entrées
+        while True:
+            try:
+                noeuds['x'][i] = float(input(f"Position x du noeud {i + 1} en {L}: "))
+            except (ValueError, SyntaxError, TypeError):
+                print('Entrée invalide, corrigez')
+                continue
+            # commence à 1 quand i est à 0
+            noeuds['ddlx'][i] = 3 * i + 1
+            try:
+                noeuds['y'][i] = float(input(f"Position y du noeud {i + 1} en {L}: "))
+            except (ValueError, SyntaxError, TypeError):
+                print('Entrée invalide, corrigez')
+                continue
+            # commence à 2 quand i est à 0
+            noeuds['ddly'][i] = 3 * i + 2
+            try:
+                noeuds['z'][i] = float(input(f"Position z du noeud {i + 1} en {L}: "))
+            except (ValueError, SyntaxError, TypeError):
+                print('Entrée invalide, corrigez')
+                continue
+                # commence à 3 quand i est à 0
+            noeuds['ddlz'][i] = 3 * i + 3
 
-ddl2 = np.array([4, 5, 6, 10, 11, 12])
-xi2, yi2, zi2 = 0, 84, 48
-xj2, yj2, zj2 = 24, 60, 48
-E2 = 30e6
-A2 = 0.2
-k2 = calculer_k_barre3d(E2, A2, xi2, yi2, zi2, xj2, yj2, zj2)
+            # Si aucun problème d'entrée, sortir de la boucle d'entrée
+            break
 
-ddl3 = np.array([7, 8, 9, 10, 11, 12])
-xi3, yi3, zi3 = 24, 60, 0
-xj3, yj3, zj3 = 24, 60, 48
-E3 = 30e6
-A3 = 0.2
-k3 = calculer_k_barre3d(E3, A3, xi3, yi3, zi3, xj3, yj3, zj3)
+    print(noeuds)
+    redo = bool(input('Appuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
+
+    redo = True
+    while redo is True:
+        try:
+            nb_elements = int(input("Combien d'éléments contient la structure? "))
+        except (ValueError, SyntaxError, TypeError):
+            continue
+        vide = [0] * int(nb_elements)
+        elements = {'ddl': vide.copy(), 'xi': vide.copy(), 'yi': vide.copy(), 'xj': vide.copy(), 'yj': vide.copy(),
+                    'E': vide.copy(), 'A': vide.copy(), 'alpha': vide.copy(), 'dT': vide.copy(),
+                    'k': vide.copy(), 'feq': vide.copy()}
+
+        for i in range(nb_elements):
+            # soustraction de 1 pour passer du numéro du noeud à son indice dans le tableau
+            while True:
+                try:
+                    noeud_i = int(input(f"\n ÉLÉMENT {i + 1}: \nNoeud avant l'élément: ")) - 1
+                    noeud_j = int(input("Noeud après l'élément: ")) - 1
+                except (ValueError, SyntaxError, TypeError):
+                    print("Valeur invalide")
+                    continue
+
+                elements['ddl'][i] = np.array(
+                    [noeuds['ddlx'][noeud_i],
+                     noeuds['ddly'][noeud_i],
+                     noeuds['ddlx'][noeud_j],
+                     noeuds['ddly'][noeud_j]])
+                elements['xi'][i], elements['yi'][i] = noeuds['x'][noeud_i], noeuds['y'][noeud_i]
+                elements['xj'][i], elements['yj'][i] = noeuds['x'][noeud_j], noeuds['y'][noeud_j]
+
+                try:
+                    print(f"\tPour un ressort, poser un module d'élasticité de 0")
+                    elements['E'][i] = eval(input(f"Module d'élasticité en {P}: "))
+                    if elements['E'][i] > 0:
+                        elements['A'][i] = eval(input(f"Aire de section en {L}^2: "))
+                    else:
+                        elements['A'][i] = float(input(f"Raideur du ressort en {F}/{L}: "))
+                    elements['dT'][i] = float(input('Différence de température: '))
+                    if elements['dT'][i] != 0:
+                        elements['alpha'][i] = float(input("Coefficient de dilatation thermique: "))
+                except (SyntaxError, ValueError, TypeError):
+                    print("Erreur dans les valeurs entrées")
+                    continue
+                elements['k'][i] = calculer_k_barre3d(elements['E'][i], elements['A'][i],
+                                                      elements['xi'][i], elements['yi'][i], elements['zi'][i],
+                                                      elements['xj'][i], elements['yj'][i], elements['zj'][i])
+                break
+
+        print(elements)
+        redo = bool(input('Appuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
+
+redo = True
+while redo is True:
+    try:
+        nb_elements = int(input("Combien d'éléments contient la structure? "))
+    except (ValueError, SyntaxError, TypeError):
+        continue
+    vide = [0] * int(nb_elements)
+    elements = {'ddl': vide.copy(), 'xi': vide.copy(), 'yi': vide.copy(), 'zi': vide.copy,
+                'xj': vide.copy(), 'yj': vide.copy(), 'zj': vide.copy(),
+                'E': vide.copy(), 'A': vide.copy(), 'alpha': vide.copy(), 'dT': vide.copy(),
+                'k': vide.copy(), 'feq': vide.copy()}
+
+    for i in range(nb_elements):
+        # soustraction de 1 pour passer du numéro du noeud à son indice dans le tableau
+        while True:
+            try:
+                noeud_i = int(input(f"\n ÉLÉMENT {i + 1}: \nNoeud avant l'élément: ")) - 1
+                noeud_j = int(input("Noeud après l'élément: ")) - 1
+            except (ValueError, SyntaxError, TypeError):
+                print("Valeur invalide")
+                continue
+
+            elements['ddl'][i] = np.array(
+                [noeuds['ddlx'][noeud_i], noeuds['ddly'][noeud_i], noeuds['ddlz'][noeud_i],
+                 noeuds['ddlx'][noeud_j], noeuds['ddly'][noeud_j], noeuds['ddlz'][noeud_j]])
+            elements['xi'][i], elements['yi'][i], elements['zi'][i] = (
+                noeuds['x'][noeud_i], noeuds['y'][noeud_i], noeuds['z'][noeud_i])
+            elements['xj'][i], elements['yj'][i], elements['zj'][i] = (
+                noeuds['x'][noeud_j], noeuds['y'][noeud_j], noeuds['z'][noeud_j])
+
+            try:
+                print(f"\tPour un ressort, poser un module d'élasticité de 0")
+                elements['E'][i] = eval(input(f"Module d'élasticité en {P}: "))
+                if elements['E'][i] > 0:
+                    elements['A'][i] = eval(input(f"Aire de section en {L}^2: "))
+                else:
+                    elements['A'][i] = float(input(f"Raideur du ressort en {F}/{L}: "))
+                elements['dT'][i] = float(input('Différence de température: '))
+                if elements['dT'][i] != 0:
+                    elements['alpha'][i] = float(input("Coefficient de dilatation thermique: "))
+            except (SyntaxError, ValueError, TypeError):
+                print("Erreur dans les valeurs entrées")
+                continue
+            elements['k'][i] = calculer_k_barre3d(elements['E'][i], elements['A'][i],
+                                                  elements['xi'][i], elements['yi'][i], elements['zi'][i],
+                                                  elements['xj'][i], elements['yj'][i], elements['zj'][i])
+            break
+
+    print(elements)
+    redo = bool(input('Appuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
 
 # ----------
 # Assemblage
 # ----------
 
-Ktot = np.zeros((12, 12))
-Ktot = assembler_matrice(Ktot, k1, ddl1, ddl1)
-Ktot = assembler_matrice(Ktot, k2, ddl2, ddl2)
-Ktot = assembler_matrice(Ktot, k3, ddl3, ddl3)
+Ktot = np.zeros((nb_noeuds * 3, nb_noeuds * 3))
+for i in range(nb_elements):
+    Ktot = assembler_matrice(Ktot, elements['k'][i], elements['ddl'][i], elements['ddl'][i])
 
 # -------------------------
 # Conditions aux frontieres
 # -------------------------
 
-ddlUc = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 11])
-Uc = np.array([[0], [0], [0], [0], [0], [0], [0], [0], [0], [-1.2]])
-ddlFc = np.array([10, 12])
-Fc = np.array([[0], [0]])
+redo = True
+while redo is True:
+    try:
+        nb_Uc = int(input('Combien de déplacements sont connus? '))
+    except (ValueError, SyntaxError, TypeError):
+        continue
+    ddlUc = [0] * nb_Uc
+    Uc = np.zeros((nb_Uc, 1))
+    for i in range(nb_Uc):
+        while True:
+            try:
+                ddlUc[i] = int(input(f'Numéro du ddl connu #{i + 1}: '))
+                Uc[i][0] = eval(input(f'Déplacement en {L} du noeud {ddlUc[i]}: '))
+            except (ValueError, SyntaxError, TypeError):
+                print('Valeur erronnée')
+                continue
+            break
+    for i in range(nb_Uc):
+        print(f'Noeud {ddlUc[i]} : {Uc[i][0]} {L}')
+    redo = bool(input('Appuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
+
+redo = True
+while redo is True:
+    nb_Fc = int(input('Combien de forces sont connues? '))
+    ddlFc = [0] * nb_Fc
+    Fc = np.zeros((nb_Fc, 1))
+    for i in range(nb_Fc):
+        while True:
+            try:
+                ddlFc[i] = int(input(f'Numéro de la force connue #{i + 1}: '))
+                Fc[i][0] = eval(input(f'Grandeur en {F} de la force {ddlFc[i]}: '))
+            except (ValueError, SyntaxError, TypeError):
+                print('Valeur erronnée')
+                continue
+            break
+    for i in range(nb_Fc):
+        print(f'Noeud {ddlFc[i]} : {Fc[i][0]:.2} {F}')
+    redo = bool(input('Appuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
 
 # ---------------
 # Partitionnement
@@ -112,8 +270,19 @@ Ftot = reconstruire_vecteur(Fc, ddlFc, Fi, ddlUc)
 # Reponses
 # --------
 
-Fy_F = extraire_vecteur(Ftot, [11])
-print('Fy_F = %.0f lbf' % Fy_F[0][0])
+tab_sigma = [0] * nb_elements
+for i in range(nb_elements):
+    tab_sigma[i] = calculer_contrainte_barre3d(Utot, elements['ddl'][i], elements['E'][i],
+                                               elements['alpha'][i], elements['dT'][i],
+                                               elements['xi'][i], elements['yi'][i], elements['zi'][i],
+                                               elements['xj'][i], elements['yj'][i], elements['zj'][i])
 
-sigma3 = calculer_contrainte_barre3d(Utot, ddl3, E3, 0, 0, xi3, yi3, zi3, xj3, yj3, zj3)
-print('sigma_FD = %.0f psi' % sigma3)
+tab_force = [0] * nb_elements
+for i in range(nb_elements):
+    tab_force[i] = elements['A'][i] * tab_sigma[i]
+
+for i in range(nb_elements):
+    print(f"Élément {i + 1}\tForce: {tab_force[i]}{F}\tContrainte: {tab_sigma[i]}{P}")
+print('\n')
+for i in range(len(Ui)):
+    print(f"Déplacement {ddlFc[i]}:\t{Ui[i]}")
