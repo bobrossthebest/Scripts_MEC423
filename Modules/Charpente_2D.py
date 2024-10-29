cbac7f375a7c6185ef18aeba8df6ce2081b8d0a5
Stidgroschest <antoine.dubuc.1@ens.etsmtl.ca>
2024-10-28 21:01:32 -0400

    conditions frontieres

--- Modules/Charpente_2D.py
+++ Modules/Charpente_2D.py
@@ -239,11 +239,56 @@ for i in range(nb_element):
 # -------------------------
 # Conditions aux frontieres
 # -------------------------
+redo = True
+while redo is True:
+    try:
+        nb_Uc = int(input('\nCombien de déplacements sont connus? '))
+    except (ValueError, SyntaxError, TypeError):
+        continue
+    ddlUc = [0] * nb_Uc
+    Uc = np.zeros((nb_Uc, 1))
+    for i in range(nb_Uc):
+        while True:
+            try:
+                ddlUc[i] = int(input(f'Numéro du ddl connu #{i + 1}: '))
+                Uc[i][0] = eval(input(f'Déplacement en {L} de U{ddlUc[i]}: '))
+            except (ValueError, SyntaxError, TypeError):
+                print('Valeur erronnée')
+                continue
+            break
+    print('\n')
+    for i in range(nb_Uc):
+        if ddlUc[i] % 3 == 0:  # Check si c'est le c'est un angle ou deplacement
+            print(f'U{ddlUc[i]} :\t{Uc[i][0]} \u03B8')
+        else:
+            print(f'U{ddlUc[i]}:\t{Uc[i][0]} {L}')
 
-ddlUc = np.array([1, 2, 3, 7, 8])
-Uc = np.array([[0], [0], [0], [0], [0]])
-ddlFc = np.array([4, 5, 6, 9])
-Fc = np.array([[0], [0], [0], [0]])
+    redo = bool(input('\nAppuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
+
+redo = True
+while redo is True:
+    try:
+        nb_Fc = int(input('Combien de forces sont connues? '))
+    except (ValueError, SyntaxError, TypeError):
+        continue
+    ddlFc = [0] * nb_Fc
+    Fc = np.zeros((nb_Fc, 1))
+    for i in range(nb_Fc):
+        while True:
+            try:
+                ddlFc[i] = int(input(f'Numéro de la force connue #{i + 1}: '))
+                Fc[i][0] = eval(input(f'Grandeur en {F} de  F{ddlFc[i]}: '))
+            except (ValueError, SyntaxError, TypeError):
+                print('Valeur erronnée')
+                continue
+            break
+    print('\n')
+    for i in range(nb_Fc):
+        if ddlFc[i] % 3 == 0:  # Check si c'est le c'est un angle ou deplacement
+            print(f'U{ddlFc[i]} :\t{Fc[i][0]} {F}*{L}')
+        else:
+            print(f'U{ddlFc[i]}:\t{Fc[i][0]} {L}')
+    redo = bool(input('\nAppuyez sur Enter pour passer à la prochaine étape, entrez 1 pour recommencer\n'))
 
 # ---------------
 # Partitionnement
